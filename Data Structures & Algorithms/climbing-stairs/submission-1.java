1bfe4c488c21803fa35b3a44011280f21be34e88
Vishnukoushik Samayamantri <vishnukoushik353@gmail.com>
2026-07-25 16:09:44 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int memo(int n,int[]dp){
+        if (n <= 1) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        dp[n] = memo(n-1,dp)+memo(n-2,dp);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return memo(n,dp);
+
+    }
+}
